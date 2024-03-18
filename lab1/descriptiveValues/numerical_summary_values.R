2f723e596d4c4900b8cafed6ea9dc21d7680f9c8
oleksandr <oleksandrlazebnij8@gmail.com>
2024-03-18 17:18:49 +0200

    summaries

---
+++ lab1/descriptiveValues/numerical_summary_values.R
@@ -0,0 +1,71 @@
+library(tidyverse)
+
+binary_variables <- c(
+  "Diabetes",
+  "HighBP", 
+  "HighChol", 
+  "CholCheck", 
+  "Smoker", 
+  "Stroke", 
+  "HeartDiseaseorAttack", 
+  "PhysActivity", 
+  "Fruits", 
+  "Veggies",
+  "HvyAlcoholConsump",
+  "AnyHealthcare",
+  "NoDocbcCost",
+  "DiffWalk"
+)
+
+category_variables <- c("Age", "Education", "Income")
+
+gen.hlth_categories <- c("excellent", "very good",
+                         "good", "fair", "poor")
+
+sex_categories <- c("female", "male")
+
+age_categories <- c("18-24", "25-29", "30-34", "35-39",
+                    "40-44", "45-49", "50-54", "55-59",
+                    "60-64", "65-69", "70-74", "75-79",
+                    "80+")
+
+edc_categories <- c("No education", 
+                    "Elementary", 
+                    "Some high school", 
+                    "High school graduate", 
+                    "Some college or tech. school", 
+                    "College graduate")
+
+income_categories <- c("Less than $10,000",
+                       "$10,000-$15,000",
+                       "$15,000-$20,000",
+                       "$20,000-$25,000",
+                       "$25,000-$35,000",
+                       "$35,000-$50,000",
+                       "$50,000-$75,000",
+                       "$75,000 or more")
+
+
+diabetes_data <- read_csv("prepared_data.csv") %>%
+  mutate(Sex = factor(Sex)) %>%
+  mutate(GenHlth = factor(GenHlth, ordered = TRUE, levels = rev(gen.hlth_categories))) %>%
+  mutate(Sex = factor(Sex)) %>%
+  mutate(Age = factor(Age, ordered = TRUE, levels = age_categories)) %>%
+  mutate(Education = factor(Education, ordered = TRUE, levels = edc_categories)) %>%
+  mutate(Income = factor(Income, ordered = TRUE, levels = income_categories)) %>%
+  mutate(across(all_of(binary_variables), as.logical))
+
+
+########################################################################################
+
+mean_bmi <- mean(diabetes_data$BMI)
+mean_ment_hlth <- mean(diabetes_data$MentHlth)
+mean_phys_hlth <- mean(diabetes_data$PhysHlth)
+
+# Создание таблицы с средними значениями
+mean_data <- data.frame(
+  Variable = c("BMI", "Mental Health", "Physical Health"),
+  Mean_Value = c(mean_bmi, mean_ment_hlth, mean_phys_hlth)
+)
+
+print(mean_data)
